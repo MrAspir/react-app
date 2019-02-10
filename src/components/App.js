aac40a583999a0db8974e7989b4db131150706b7
Roman Krasinskyi <krasinskiy.r.i@gmail.com>
2019-02-10 21:34:51 +0200

    Added fullscreen background slideshow

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,64 @@
 import React from 'react';
 
+import BackgroundSlider from 'react-background-slider'
+
 import Header from './Header';
 import CitiesPage from './CitiesPage';
 import Footer from './Footer';
 
-const App = () => (
-    <div className="wrapper">
-        <Header />
+import slide1 from '../images/slide1.jpg'
+import slide2 from '../images/slide2.jpg'
+import slide3 from '../images/slide3.jpg'
+import slide4 from '../images/slide4.jpg'
 
-        <div className="main">
-            <div className="container">
-                <div className="row">
-                    <CitiesPage />
+const App = () => {
+    /*const images = [slide1, slide2, slide3, slide4];
+
+    let number = 0;
+
+    let wrapperStyle = {
+        backgroundImage: `url(${images[number]})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover'
+    };
+
+    setInterval(() => {
+        console.log(number);
+
+        number += 1;
+
+        if (number > (images.length - 1)) {
+            number = 0;
+        }
+
+        wrapperStyle = {
+            backgroundImage: `url(${images[number]})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover'
+        };
+        console.log(wrapperStyle);
+
+    }, 5000);*/
+
+    return (
+        <div className="wrapper">
+            <BackgroundSlider
+                images={[slide1, slide2, slide3, slide4]}
+                duration={10} transition={2} />
+
+            <Header />
+
+            <div className="main">
+                <div className="container">
+                    <div className="row">
+                        <CitiesPage />
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <Footer />
-    </div>
-);
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
